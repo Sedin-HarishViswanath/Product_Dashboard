8edc1570d657b107f4cf04e0fb896ced530a3aac
sedin-liveshm <livesh.m@sedintechnologies.com>
2026-04-21 17:01:30 +0530

    finished pipes

--- src/app/pipes/currency-formatter-pipe.ts
+++ src/app/pipes/currency-formatter-pipe.ts
@@ -2,9 +2,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'currencyFormatter',
+  standalone: true,
+  pure: true,
 })
 export class CurrencyFormatterPipe implements PipeTransform {
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: number, currencyCode = 'INR', locale = 'en-IN'): string {
+    return new Intl.NumberFormat(locale, {
+      style: 'currency',
+      currency: currencyCode,
+      maximumFractionDigits: 0,
+    }).format(value);
   }
 }
