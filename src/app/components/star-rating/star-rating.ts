57cdac039017707e53edb4c4c9deddca5d248ebf
sedin-liveshm <livesh.m@sedintechnologies.com>
2026-04-21 16:48:37 +0530

    finished star-rating component

--- src/app/components/star-rating/star-rating.ts
+++ src/app/components/star-rating/star-rating.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-star-rating',
-  imports: [],
+  imports: [CommonModule],
+  standalone: true,
   templateUrl: './star-rating.html',
   styleUrl: './star-rating.css',
 })
-export class StarRating {}
+export class StarRating {
+  @Input() rating = 0;
+  @Output() rated = new EventEmitter<number>();
+
+  readonly stars = [1, 2, 3, 4, 5];
+
+  setRating(star: number): void {
+    this.rating = star;
+    this.rated.emit(star);
+  }
+}
