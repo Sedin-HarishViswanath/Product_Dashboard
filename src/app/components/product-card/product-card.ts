c6dee0398236c7a471da4fcf275d41ba7a4d2b08
Sedin-HarishViswanath <harish.viswanath@sedintechnologies.com>
2026-04-21 17:09:24 +0530

    Finished product-card component

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { CurrencyFormatterPipe } from '../../pipes/currency-formatter-pipe';
+import { TimeAgoPipe } from '../../pipes/time-ago-pipe';
+import { StarRating } from '../star-rating/star-rating';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule, StarRating, CurrencyFormatterPipe, TimeAgoPipe],
   templateUrl: './product-card.html',
   styleUrl: './product-card.css',
 })
-export class ProductCard {}
+export class ProductCard {
+  @Input({ required: true }) product!: Product;
+  @Output() ratingChange = new EventEmitter<{ productId: number; rating: number }>();
+
+  get isLowStock(): boolean {
+    return this.product.stockCount < 5;
+  }
+
+  onRateProduct(rating: number): void {
+    this.ratingChange.emit({ productId: this.product.id, rating });
+  }
+}
